c46997a7df671eda4d5873945b67ae2df9180499
TheUncas <david21.martins@gmail.com>
2019-02-09 23:24:53 +0100

    DarkRift tutorial Part4
    DarkRift tutorial Part4

---
+++ Assets/Scripts/Network/GameServerManager.cs
@@ -0,0 +1,67 @@
+﻿using DarkRift.Server;
+using DarkRift.Server.Unity;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameServerManager : MonoBehaviour
+{
+
+    #region Properties
+
+    /// <summary>
+    /// List of connected clients
+    /// </summary>
+    public List<int> clientsId;
+
+    /// <summary>
+    /// Reference to the DarkRift server
+    /// </summary>
+    public XmlUnityServer serverReference;
+
+    #endregion
+
+    #region Unity Callbacks
+    private void Start()
+    {
+        //////////////////
+        /// Properties initialization
+        clientsId = new List<int>();
+        serverReference = GetComponent<XmlUnityServer>();
+
+        //////////////////
+        /// Events subscription
+        serverReference.Server.ClientManager.ClientConnected += ClientConnected;
+        serverReference.Server.ClientManager.ClientDisconnected += ClientDisconnected;
+
+        
+    }
+
+
+    
+    #endregion
+
+    #region Server events
+    /// <summary>
+    /// When a client connects to the DarkRift server
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ClientConnected(object sender, ClientConnectedEventArgs e)
+    {
+        clientsId.Add(e.Client.ID);
+    }
+
+    /// <summary>
+    /// When a client disconnects to the DarkRift server
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ClientDisconnected(object sender, ClientDisconnectedEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+    #endregion
+
+}
