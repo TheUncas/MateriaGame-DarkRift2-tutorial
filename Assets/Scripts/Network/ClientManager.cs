3034f9c527b77e18e842f4bfe2130941c15d6f9e
TheUncas <david21.martins@gmail.com>
2019-02-10 21:44:19 +0100

    DarkRift tutorial Part 6

---
+++ Assets/Scripts/Network/ClientManager.cs
@@ -0,0 +1,30 @@
+﻿using DarkRift.Client.Unity;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClientManager : MonoBehaviour
+{
+
+    #region Properties
+
+    /// <summary>
+    /// Reference to the DarkRift2 client
+    /// </summary>
+    public UnityClient clientReference;
+
+    #endregion
+
+    #region Unity Callbacks
+    // Start is called before the first frame update
+    void Start()
+    {
+        //////////////////
+        /// Properties initialization
+        clientReference = GetComponent<UnityClient>();
+
+        //////////////////
+        /// Load the game scene
+        SceneManager.LoadScene("MainGameScene", LoadSceneMode.Additive);
+    }
+    #endregion
+}
